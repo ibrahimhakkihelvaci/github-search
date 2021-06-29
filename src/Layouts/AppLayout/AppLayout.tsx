c575cd39fefabccd0432c23c6ab2837f6e22a632
ibrahimhakkihelvaci <66227589+ibrahimhakkihelvaci@users.noreply.github.com>
2021-06-30 00:07:30 +0300

    updated appBar

--- src/Layouts/AppLayout/AppLayout.tsx
+++ src/Layouts/AppLayout/AppLayout.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react";
 
 //MUI Comps
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, fade } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -14,6 +14,12 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
 import MailIcon from "@material-ui/icons/Mail";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+
+//Utils
+import colors from "../../utils/colors";
+import logo from "../../utils/images/digieggs-logo.png";
 
 const drawerWidth = 240;
 
@@ -23,6 +29,8 @@ const useStyles = makeStyles((theme) => ({
 	},
 	appBar: {
 		zIndex: theme.zIndex.drawer + 1,
+		backgroundColor: colors.darkBlue,
+		display: "flex",
 	},
 	drawer: {
 		width: drawerWidth,
@@ -38,6 +46,47 @@ const useStyles = makeStyles((theme) => ({
 		flexGrow: 1,
 		padding: theme.spacing(3),
 	},
+	search: {
+		position: "relative",
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: "#19417f",
+		"&:hover": {
+			backgroundColor: fade(theme.palette.common.white, 0.25),
+		},
+		marginLeft: 0,
+		width: 288,
+	},
+	searchIcon: {
+		padding: theme.spacing(0, 2),
+		height: "100%",
+		position: "absolute",
+		pointerEvents: "none",
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	inputRoot: {
+		color: "inherit",
+	},
+	inputInput: {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+		transition: theme.transitions.create("width"),
+		width: "100%",
+		[theme.breakpoints.up("md")]: {
+			width: "20ch",
+		},
+	},
+	logo: {
+		width: 138,
+		height: 21,
+	},
+	appBarContent: {
+		display: "flex",
+		alignItems: "center",
+		width: "auto",
+	},
 }));
 
 const AppLayout: FunctionComponent = (props) => {
@@ -49,11 +98,24 @@ const AppLayout: FunctionComponent = (props) => {
 		<div className={classes.root}>
 			<CssBaseline />
 			<AppBar position='fixed' className={classes.appBar}>
-				<Toolbar>
-					<Typography variant='h6' noWrap>
-						Clipped drawer
-					</Typography>
-				</Toolbar>
+				<div className={classes.appBarContent}>
+					<Toolbar>
+						<img src={logo} className={classes.logo} />
+					</Toolbar>
+					<div className={classes.search}>
+						<div className={classes.searchIcon}>
+							<SearchIcon />
+						</div>
+						<InputBase
+							placeholder='Search…'
+							classes={{
+								root: classes.inputRoot,
+								input: classes.inputInput,
+							}}
+							inputProps={{ "aria-label": "search" }}
+						/>
+					</div>
+				</div>
 			</AppBar>
 			<Drawer
 				className={classes.drawer}
---
+++ src/utils/colors.js
@@ -0,0 +1,4 @@
+const colors = {
+	darkBlue: "#375f9d",
+};
+export default colors;
